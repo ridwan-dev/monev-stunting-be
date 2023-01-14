84b75160473bcb8f9f372e6a4ecbc9ea73516a91
ridwan.develop <ridwan.develop@gmail.com>
2023-01-15 02:24:07 +0700

    update

---
+++ app/Http/Controllers/Api/V3/KinerjaPembangunan/KinerjaPembangunanController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api\V3\KinerjaPembangunan;
+
+use App\Http\Controllers\Api\BaseController;
+use Illuminate\Http\Request;
+use App\Models\V3\KinerjaPembangunan\KinerjaPembangunanLokus ;
+use App\Models\V3\KinerjaPembangunan\KinerjaPembangunanRoIstilah ;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
+
+class KinerjaPembangunanController extends BaseController
+{
+   public function lokusRo(Request $request){
+      $tahun = now()->year;
+      $bulan = now()->month;
+      $semester = ($bulan/6) <= 6 ? 1 : 2;
+      $ro = [];
+      if($request->has('tahun') && !empty($request->tahun)){
+         $tahun = $request->tahun;
+      }
+
+      if($request->has('semester') && !empty($request->semester)){
+         $semester = $request->semester;
+      } 
+
+      if($request->has('ro') && !empty($request->ro)){
+            $ro = $request->ro;
+      }
+
+      $dataKinerjaPembangunan = KinerjaPembangunanLokus::where(function($q) use($tahun, $bulan, $semester, $ro){
+            if($tahun != "all"){
+               $q->where('tahun', $tahun);
+            }
+
+            if($semester != "all"){
+               $q->where('semester', $semester);
+            }
+
+            if(count($ro)<58){
+               foreach($ro as $rw){
+                  $q->where($rw, "Y");
+               }               
+            }
+         })->join('spatial_data.peta_lokasi', function ($join) {
+            $join->on('spatial_data.peta_lokasi.kode_bps', '=', 'versi_tiga.kinerja_pembangunan.kab_id')
+               ->where('level', '=', 'district');
+      })
+      ->select(['versi_tiga.kinerja_pembangunan.*','spatial_data.peta_lokasi.shape'])
+      ->get();
+      
+      $result = [
+         "data" => $dataKinerjaPembangunan,
+         "field" => KinerjaPembangunanRoIstilah::all(),
+      ];
+      return $this->returnJsonSuccess("Data fetched successfully", $result);
+   }
+
+}
---
+++ routes/api/v3/api.php
@@ -0,0 +1,28 @@
+<?php
+
+//use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use \App\Faker;
+use \App\JsonResponse;
+use \App\Constants;
+
+use App\Http\Controllers\Api\V3\{
+    KinerjaPembangunan\KinerjaPembangunanController
+};
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('lokus-ro', [KinerjaPembangunanController::class, 'lokusRo']);
+    
+
+
+});
