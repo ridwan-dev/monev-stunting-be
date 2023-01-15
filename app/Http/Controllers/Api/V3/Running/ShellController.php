9332753c380a40cbc0480b4f1ee0a41be9c10f8d
ridwan.develop <ridwan.develop@gmail.com>
2023-01-15 12:32:06 +0700

    update

--- app/Http/Controllers/Api/V3/Running/ShellController.php
+++ app/Http/Controllers/Api/V3/Running/ShellController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 class ShellController extends BaseController
 {
    public function terminal(){
-      $this->commandFunc("cd ../exexcute.sh");
+      $this->commandFunc("../exexcute.sh");
       //$this->commandFunc("git config --global --add safe.directory /var/www/html/api-dev-v3");
       //$this->commandFunc("git pull origin develop");
       $this->commandFunc("cd /var/www/html/testing-v3");
