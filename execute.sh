6d149892f1b61912b1df998c4e46c9466bb1b739
Monev Stunting KGM Bappenas <ridwan.develop@gmail.com>
2023-01-15 13:18:29 +0700

    xxx

--- execute.sh
+++ execute.sh
@@ -1,7 +1,8 @@
-cd /var/www/html/api-uat
+cd /var/www/html/api-dev-v3
+git config --global --add safe.directory /var/www/html/api-dev-v3
 git pull
-composer install
-cd /var/www/html/api-dev
-git pull
-composer install
+#composer install
+#cd /var/www/html/api-dev
+#git pull
+#composer install
 exit 
