2fe7fa2cf43db6dde460809ee813a1d694e27ae7
ridwan.develop <ridwan.develop@gmail.com>
2023-01-26 10:23:16 +0700

    update

--- execute.sh
+++
@@ -1,8 +0,0 @@
-cd /var/www/html/api-dev-v3
-git config --global --add safe.directory /var/www/html/api-dev-v3
-git pull
-#composer install
-#cd /var/www/html/api-dev
-#git pull
-#composer install
-exit 
